42a4ae60e9f7b6503faa6eb6ad69b4ec299bfa18
Bonita Choi <bonitac93@gmail.com>
2019-03-11 23:49:36 +0000

    function to parse json into array of objects, and print the avatar urls

--- download_avatars.js
+++ download_avatars.js
@@ -1,17 +1,24 @@
 require('dotenv').config();
+var sk = require('./secrets')
 var request = require('request');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  var url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
-  request(url, function(err, res, body) {
-    cb(err, body);
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': sk
+    }
+  };
+
+  request(options, function(err, res, body) {
+    cb(err, JSON.parse(body));
+    JSON.parse(body).forEach(function(user){console.log(user.avatar_url)
   });
-}
+})}
 //cb is ballback function to handle asynch of results to be returned from getRepoContri
 //deal with err after establishing "happy path"
 
 
 getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
 });
