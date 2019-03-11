e21afc2fbddfbe9c54d816975d3eb17807d0e2ae
Bonita Choi <bonitac93@gmail.com>
2019-03-11 21:46:45 +0000

    setting up getRepoContributors function

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,13 @@
 var request = require('request');
 
-console.log('Welcome to the GitHub Avatar Downloader!');
+function getRepoContributors(repoOwner, repoName, cb) {
+  // ...
+}
+//cb is ballback function to handle asynch of results to be returned from getRepoContri
+//deal with err after establishing "happy path"
+
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
