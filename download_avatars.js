c2cf3ba701625a04c3c63d4b1071bed411d19cae
Bonita Choi <bonitac93@gmail.com>
2019-03-11 23:22:08 +0000

    added env

--- download_avatars.js
+++ download_avatars.js
@@ -1,7 +1,11 @@
+require('dotenv').config();
 var request = require('request');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+  var url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
+  request(url, function(err, res, body) {
+    cb(err, body);
+  });
 }
 //cb is ballback function to handle asynch of results to be returned from getRepoContri
 //deal with err after establishing "happy path"
