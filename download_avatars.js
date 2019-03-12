62e5dd68c381a67977a2b8792925f41fca2ccd82
Bonita Choi <bonitac93@gmail.com>
2019-03-12 00:21:50 +0000

    making folder using downloadImageByURL

--- download_avatars.js
+++ download_avatars.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 var sk = require('./secrets')
 var request = require('request');
+var fs = require('fs')
 
 function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
@@ -10,15 +11,35 @@ function getRepoContributors(repoOwner, repoName, cb) {
       'Authorization': sk
     }
   };
-
+  
   request(options, function(err, res, body) {
-    cb(err, JSON.parse(body));
+    cb(err, JSON.parse(body)); //deal with err after establishing "happy path"
     JSON.parse(body).forEach(function(user){console.log(user.avatar_url)
-  });
-})}
-//cb is ballback function to handle asynch of results to be returned from getRepoContri
-//deal with err after establishing "happy path"
+    });
+  })}
+//cb is callback function to handle asynch of results to be returned from getRepoContri
+
+function downloadImageByURL(url, filePath) {
+  fs.mkdir("avatars",function(err){ //async making folder and then making sure the request comes after
+    //add error if directory somehow doesn't get made
+    request.get(url)
+    .on('error', function (err) {
+      throw err; 
+    })
+    .on('response', function (response) {
+      console.log('Response Status Code: ', response);
+    })
+    .pipe(fs.createWriteStream("./"+filePath));  
+    })
+}
+downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg")
+
+// request.get("https://avatars2.githubusercontent.com/u/2741?v=3&s=466")
+//         .on('error', function (err){throw err})
+//         .pipe(fs.createReadStream("test.jpg"))
 
 
-getRepoContributors("jquery", "jquery", function(err, result) {
-});
+
+
+// getRepoContributors("jquery", "jquery", function(err, result) {
+// });
